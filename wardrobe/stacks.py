d4d2fe51402dc220ec158fa59c72ed7eadf36604
Benoit Bryon <benoit@marmelune.net>
2012-07-12 10:14:00 +0200

    Added some inline documentation and doctests. Fixed copy and comparison operators. Improved constructor.

--- wardrobe/stacks.py
+++ wardrobe/stacks.py
@@ -8,9 +8,15 @@ class PopException(Exception):
 
 
 class StackedDict(object):
-    """Dictionary of stacks.
+    """Dictionary-like object made of stacked layers.
 
-    Calls to push() or pop() affect one entire layer of the stack.
+    Instances act like dictionaries.
+
+    Use :py:meth:`push` to take snapshots of the data, then :py:meth:`pop` to
+    restore the last snapshot.
+
+    Calls to push() or pop() affect (respectively create or delete) one entire
+    layer of the stack.
 
     >>> from wardrobe import StackedDict
     >>> clark = StackedDict()
@@ -42,24 +48,58 @@ class StackedDict(object):
     'Lois'
 
     """
-    def __init__(self):
-        self._reset_stack()
+    def __init__(self, initial=None, **kwargs):
+        """Constructor.
+
+        >>> sd = StackedDict()
+        >>> list(sd._stack)
+        [{}]
+        >>> sd = StackedDict({'a': 1})
+        >>> list(sd._stack)
+        [{'a': 1}]
+        >>> sd = StackedDict(a=1)
+        >>> list(sd._stack)
+        [{'a': 1}]
+
+        """
+        if initial is None:
+            if kwargs:
+                initial = kwargs
+            else:
+                initial = {}
+        self._reset_stack(initial)
 
     def _reset_stack(self, initial={}):
-        self.stack = deque([initial])
+        """Initialize data."""
+        self._stack = deque([initial])
 
     def __copy__(self):
+        """Copy operator.
+        
+        >>> from copy import copy
+        >>> right = StackedDict()
+        >>> left = right
+        >>> left is right
+        True
+        >>> right = StackedDict()
+        >>> left = copy(right)
+        >>> left is right
+        False
+        >>> left == right
+        True
+
+        """
         duplicate = copy(super(StackedDict, self))
-        duplicate.stack = self.stack[:]
+        duplicate._stack = copy(self._stack)
         return duplicate
 
     def __len__(self):
-        return len(self.stack)
+        return len(self._stack)
 
     def __getitem__(self, key):
         """Get a variable's value, starting at the current layer and going
         upward."""
-        for layer in self.stack:
+        for layer in self._stack:
             try:
                 return layer[key]
             except KeyError:
@@ -67,26 +107,29 @@ class StackedDict(object):
         raise KeyError(key)
 
     def __setitem__(self, key, value):
-        self.stack[0][key] = value
+        self._stack[0][key] = value
 
     def __delitem__(self, key):
-        del self.stack[0][key]
+        del self._stack[0][key]
 
     def __iter__(self):
-        return iter(self.stack)
+        return iter(self._stack)
 
     def push(self):
         layer = {}
-        self.stack.appendleft(layer)
+        self._stack.appendleft(layer)
 
     def pop(self):
-        return self.stack.popleft()
+        return self._stack.popleft()
 
-    def has_key(self, key):
-        for layer in self.stack:
+    def _has_key(self, key):
+        for layer in self._stack:
             if key in layer:
                 return True
         return False
 
     def __contains__(self, key):
-        return self.has_key(key)
+        return self._has_key(key)
+
+    def __cmp__(self, other):
+        return cmp(self._stack, other._stack)
